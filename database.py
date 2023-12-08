890e3e09b86fcb650bd5b90f574e1e284d92696d
Sebastian de la Fuente <sebadlf@gmail.com>
2023-12-07 21:26:20 -0300

    format project

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-DATABASE_URL = 'postgres://user:JqzFXy7c3JSFebDsVAaVElVBSTlRsTDL@dpg-clp5rhp46foc73cd9es0-a/todos_v0uv'
+DATABASE_URL = 'postgresql+psycopg2://user:JqzFXy7c3JSFebDsVAaVElVBSTlRsTDL@dpg-clp5rhp46foc73cd9es0-a/todos_v0uv'
 
 engine = create_engine(DATABASE_URL)
 
