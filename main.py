afcd2c666d3d2b1ef198b23a4a492ebebeb9d8d9
Sebastian de la Fuente <sebadlf@gmail.com>
2023-12-28 12:25:26 -0300

    cors

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 load_dotenv() 
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from fastapi import FastAPI
 import models
 from database import engine
@@ -9,5 +11,14 @@ from routers import todos
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:5173'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(todos.router)
